c94d58e1faafca49a904d3153d32b6b032ba99c3
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-29 23:55:01 +0200

    add countdown functionality

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 let dateTime = luxon.DateTime;
-let now = dateTime.now();
-let concertDate = dateTime.local(2024, 11, 25);
-let interval = luxon.Interval.fromDateTimes(now, concertDate);
-
-console.log(interval.length("days"));
+let concertDate = dateTime.local(2024, 11, 25, 18, 30);
 
 let dateElement = document.querySelector("#date-of-concert");
-dateElement.innerHTML = `${concertDate.toLocaleString()}`;
+dateElement.innerHTML = `${concertDate.toFormat("DDD")}`;
+
+function updateCountdown() {
+  let now = dateTime.now();
+  let interval = luxon.Interval.fromDateTimes(now, concertDate);
+  let duration = interval.toDuration(["days", "hours", "minutes", "seconds"]);
+  let countdownElement = document.querySelector("#time");
+  let daysUntilElement = document.querySelector("#days");
+
+  daysUntilElement.innerHTML = `${Math.floor(interval.length("days"))} days`;
+  countdownElement.innerHTML = `<strong>${
+    duration.toObject().hours
+  }</strong> <small>hours,</small> <strong>${
+    duration.toObject().minutes
+  }</strong><small> minutes and</small> <strong>${Math.floor(
+    duration.toObject().seconds
+  )}</strong> <small>seconds left</small>`;
+}
+updateCountdown();
+setInterval(updateCountdown, 1000);
